842ae690b5b04e6d8f63f29890ea3bc210cafbae
HadiM <marelo64@gmail.com>
2014-02-12 13:20:00 +0100

    unrelease 0.1

--- 0.1/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:43,titleterms:{tracker:0,tabl:0,indic:0,document:0,welcom:0,scikit:0},terms:{index:0,content:0,test2:0,page:0,modul:0,todo:0,search:0},objects:{},titles:["Welcome to scikit-tracker&#8217;s documentation!"],filenames:["index"],objnames:{},objtypes:{}})
