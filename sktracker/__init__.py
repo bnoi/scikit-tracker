ed5b70bf3d4b2ecf71d540687b49c3cb3c246b7b
HadiM <marelo64@gmail.com>
2014-02-12 11:06:10 +0100

    init package and add version 0.1-dev

---
+++ sktracker/__init__.py
@@ -0,0 +1,18 @@
+"""Object detection and tracking for cell biology
+
+``sklearn`` is bla bla bla.
+
+Subpackages
+-----------
+color
+    Color space conversion.
+
+
+Utility Functions
+-----------------
+img_as_float
+    Convert an image to floating point format, with values in [0, 1].
+
+"""
+
+from .version import __version__
