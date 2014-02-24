3844c3e77da57b001ca55a9ae8eb34a08313728a
HadiM <marelo64@gmail.com>
2014-02-24 12:49:41 +0100

    Add setup_logging during sktracker init

--- sktracker/__init__.py
+++ sktracker/__init__.py
@@ -4,10 +4,12 @@
 
 Subpackages
 -----------
-color
-    Color space conversion.
+utils
+    Utilities functions
 
 """
+
+import logging
 
 try:
     from .version import __version__
@@ -15,3 +17,31 @@ except ImportError: # pragma: no cover
     __version__ = "dev" # pragma: no cover
 
 from . import utils
+
+def setup_log(): # pragma: no cover
+
+    from .utils import color
+    from .utils import in_ipython
+
+    if in_ipython():
+        logformat = '%(asctime)s' + ':'
+        logformat += '%(levelname)s' + ':'
+        logformat += '%(name)s' + ':'
+        # logformat += '%(funcName)s' + ': '
+        logformat += ' %(message)s'
+    else:
+        logformat = color('%(asctime)s', 'BLUE') + ':'
+        logformat += color('%(levelname)s', 'RED') + ':'
+        logformat += color('%(name)s', 'YELLOW') + ':'
+        # logformat += color('%(funcName)s', 'GREEN') + ': '
+        logformat += color(' %(message)s', 'ENDC')
+
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(logformat, "%Y-%m-%d %H:%M:%S")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+setup_log()
