d32b929348812585d5917a7b036717591f12aedb
HadiM <marelo64@gmail.com>
2014-03-13 11:53:18 +0100

    New documentation theme: sphinx_rtd_theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,12 +26,12 @@ curpath = os.path.dirname(__file__)
 # Import numpydoc
 sys.path.append(os.path.join(curpath, '..', 'extensions', 'numpydoc'))
 
-# Import sphinx_bootstrap_theme
-sys.path.append(os.path.join(curpath, '..', 'extensions', 'sphinx-bootstrap-theme'))
-import sphinx_bootstrap_theme
-
 # Import sktracker
 sys.path.append(os.path.join(curpath, '..', '..'))
+
+# Import theme
+sys.path.append(os.path.join(curpath, '..', 'extensions', 'sphinx_rtd_theme'))
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -123,7 +123,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -131,7 +131,7 @@ html_theme = 'bootstrap'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -298,73 +298,3 @@ intersphinx_mapping = {
 # -----------------------------------------------------------------------------
 numpydoc_show_class_members = False
 
-# -----------------------------------------------------------------------------
-# Sphinx Bootstrap Theme configuration
-# -----------------------------------------------------------------------------
-
-# (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
-# Path should be relative to the ``_static`` files directory.
-html_logo = ""
-
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.
-html_theme_options = {
-    # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "Scikit-tracker",
-
-    # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': "Content",
-
-    # A list of tuples containing pages or urls to link to.
-    # Valid tuples should be in the following forms:
-    #    (name, page)                 # a link to a page
-    #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
-    #    (name, "http://example.com", True) # arbitrary absolute url
-    # Note the "1" or "True" value above as the third argument to indicate
-    # an arbitrary url.
-    'navbar_links': [
-        #("Examples", "examples"),
-        #("Link", "http://example.com", True),
-    ],
-
-    # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': True,
-
-    # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': False,
-
-    # Global TOC depth for "site" navbar tab. (Default: 1)
-    # Switching to -1 shows all levels.
-    'globaltoc_depth': 2,
-
-    # Include hidden TOCs in Site navbar?
-    #
-    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
-    # non-hidden ``toctree`` directives in the same page, or else the build
-    # will break.
-    #
-    # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
-
-    # HTML navbar class (Default: "navbar") to attach to <div> element.
-    # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar navbar-inverse",
-
-    # Fix navigation bar to top of page?
-    # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
-
-    # Location of link to source.
-    # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "nav",
-
-    # Bootswatch (http://bootswatch.com/) theme.
-    #
-    # Options are nothing with "" (default) or the name of a valid theme
-    # such as "amelia" or "cosmo".
-    'bootswatch_theme': "lumen",
-
-    # Choose Bootstrap version.
-    # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
-}
