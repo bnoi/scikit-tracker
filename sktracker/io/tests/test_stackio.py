3fbc874da9f7dcde3f1f60a43cb84df328b821c5
HadiM <marelo64@gmail.com>
2014-03-24 12:53:51 +0100

    Add FileName metadata

--- sktracker/io/tests/test_stackio.py
+++ sktracker/io/tests/test_stackio.py
@@ -1,22 +1,24 @@
-import os
+from sktracker import data
 
-from nose import with_setup
+# from nose import with_setup
 
-data_dir = os.path.dirname(os.path.realpath(__file__))
+# def open_tubhiswt_4D_ome_xml():
+#     fname = "tubhiswt-4D.ome.xml"
+#     f = open(os.path.join(current_dir, fname))
+#     global xml_str
+#     xml_str = f.read()
+#     f.close()
+#     return xml_str
 
-def open_tubhiswt_4D_ome_xml():
-    fname = "tubhiswt-4D.ome.xml"
-    f = open(os.path.join(current_dir, fname))
-    global xml_str
-    xml_str = f.read()
-    f.close()
-    return xml_str
-
-def xml_str_clean():
-    xml_str = None
+# def xml_str_clean():
+#     xml_str = None
 
 # @with_setup(setup=open_tubhiswt_4D_ome_xml, teardown=xml_str_clean)
 # def test_ome_loading():
 
 #     m = OMEModel(xml_str)
 #     del m
+
+def test_stackio():
+
+    st = StackIO
