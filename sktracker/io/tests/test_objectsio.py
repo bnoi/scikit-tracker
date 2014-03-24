50e8f6e540bb8e94b61a0ee10b966b5b5c00f930
Guillaume Gay <guillaume@mitotic-machine.org>
2014-03-24 16:52:14 +0100

    test objectsio

--- sktracker/io/tests/test_objectsio.py
+++ sktracker/io/tests/test_objectsio.py
@@ -5,10 +5,11 @@ from nose import with_setup
 from  sktracker import data
 
 
-from sktracker.io import objectsio
+from sktracker.io import objectsio, validate_metadata
 
 def test_from_store():
 
     store_path = data.sample_h5()
+    oio = objectsio.ObjectsIO.from_h5(store_path)
+    assert validate_metadata(oio.metadata)
 
-    
