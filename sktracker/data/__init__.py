2c77f47862ccbd89fbc75a034c1d8cfb7304b085
HadiM <marelo64@gmail.com>
2014-03-24 12:38:34 +0100

    Add data getter and fix test to use them

--- sktracker/data/__init__.py
+++ sktracker/data/__init__.py
@@ -1,3 +1,15 @@
 import os
 
-path = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.dirname(os.path.realpath(__file__))
+
+def CZT_peaks():
+    return os.path.join(data_path, "CZT_peaks.ome.tif")
+
+def sample():
+    return os.path.join(data_path, "sample.ome.tif")
+
+def tubhiswt_4D():
+    return os.path.join(data_path, "tubhiswt-4D.ome.xml")
+
+def metadata_json():
+    return os.path.join(data_path, "metadata.json")
