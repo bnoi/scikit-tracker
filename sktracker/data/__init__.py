149f735f4c2f1a916817bd920a66f2614b1dae5f
HadiM <marelo64@gmail.com>
2014-03-25 12:00:38 +0100

    Add data TZ_nucleus.ome.tif

--- sktracker/data/__init__.py
+++ sktracker/data/__init__.py
@@ -20,8 +20,11 @@ def sample_h5():
 
 def stack_list_dir():
     return os.path.join(data_path, "stack_list")
-    
+
 def stack_list():
     dirname = stack_list_dir()
     file_list = load_img_list(dirname)
     return file_list
+
+def TZ_nucleus():
+    return os.path.join(data_path, "TZ_nucleus.ome.tif")
