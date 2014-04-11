fd864b239836583de3f95cc3ed1c219e44df1b30
Guillaume Gay <guillaume@mitotic-machine.org>
2014-04-11 12:00:47 +0200

    prepare for merge with interpolation branch

--- sktracker/trajectories/draw.py
+++ sktracker/trajectories/draw.py
@@ -110,6 +110,7 @@ def show_4panels(trajs, label, coords=('x', 'y', 'z'),
 
     if interpolate:
         raise NotImplementedError
+        # segment_i = trajs.time_interpolate(**interp_kw).get_segments()[label]
     else:
         segment_i = segment
 
