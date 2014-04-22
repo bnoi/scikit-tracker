96dee5898a0f16e7f955e50d764517eb242a73d8
Guillaume Gay <guillaume@mitotic-machine.org>
2014-04-22 14:16:32 +0200

    undo changes in tifffile.py

--- sktracker/io/tifffile.py
+++ sktracker/io/tifffile.py
@@ -1,13 +1,3 @@
-
-# -*- coding: utf-8 -*-
-
-
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # tifffile.py
