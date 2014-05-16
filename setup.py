bbd7a416f80d6975e2c90ce19f0966adde18edaf
Guillaume Gay <guillaume@mitotic-machine.org>
2014-05-16 18:23:10 +0200

    new test file in data/stack_list and the correspondig entry in setup.py

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 
         packages=find_packages(),
         package_data={
-            '': ['data/*.h5', 'data/*.xml', 'data/*.tif', 'data/stack_list/*.tif'],
+            '': ['data/*.h5', 'data/*.xml', 'data/*.tif', 'data/stack_list/*.tif', 'data/stack_list/*.h5'],
         },
 
         tests_require='nose',
