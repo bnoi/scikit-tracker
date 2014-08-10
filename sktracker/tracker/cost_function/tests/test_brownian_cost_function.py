aedab72803114839b01c4f76b39ff34f5641a6c1
Hadrien Mary <hadrien.mary@gmail.com>
2014-08-10 15:53:46 +0200

    Add real test for BrownianGapCloseCostFunction

--- sktracker/tracker/cost_function/tests/test_brownian_cost_function.py
+++ sktracker/tracker/cost_function/tests/test_brownian_cost_function.py
@@ -1,12 +1,9 @@
-
 # -*- coding: utf-8 -*-
-
 
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
-
 
 import numpy as np
 from numpy.testing import assert_array_almost_equal
@@ -15,6 +12,7 @@ from sktracker import data
 
 from sktracker.tracker.cost_function.brownian import BrownianLinkCostFunction
 from sktracker.tracker.cost_function.brownian import BrownianGapCloseCostFunction
+from sktracker.trajectories import Trajectories
 
 
 def test_brownian_link():
@@ -43,4 +41,27 @@ def test_brownian_link():
 
 def test_brownian_gap_close():
 
-    cost_func = BrownianGapCloseCostFunction(parameters={'max_speed': 5.})
+    trajs = Trajectories(data.with_gaps_df())
+    max_speed = 10.
+
+    cost_func = BrownianGapCloseCostFunction(parameters={'max_speed': max_speed})
+
+    in_idxs = [(3, 0), (3, 0), (5, 1), (13, 2), (13, 2), (16, 3)]
+    out_idxs = [(5, 3), (7, 4), (7, 4), (15, 5), (18, 6), (18, 6)]
+
+    cost_func.context['trajs'] = trajs
+    cost_func.context['idxs_in'] = in_idxs
+    cost_func.context['idxs_out'] = out_idxs
+
+    cost_func.get_block()
+    m = cost_func.mat
+
+    m_true = [[np.nan, np.nan, np.nan, 2.0553543815820543, 45.74823093541038, np.nan, np.nan],
+              [np.nan, np.nan, np.nan, np.nan, 1.1530875267623917, np.nan, np.nan],
+              [np.nan, np.nan, np.nan, np.nan, np.nan, 0.8254178191605767, 23.84513283483594],
+              [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 4.916599888362159],
+              [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+              [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+              [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]]
+
+    assert_array_almost_equal(m, m_true)
