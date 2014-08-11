aec2eade6d67f6b31ce087931383f5b66d1bb84e
Hadrien Mary <hadrien.mary@gmail.com>
2014-08-11 17:27:01 +0200

    More tests to get Travis ok

--- doc/convert_notebooks.sh
+++ doc/convert_notebooks.sh
@@ -7,5 +7,6 @@ FILES=$(find . -name "*.ipynb" -not -path "*.ipynb_checkpoints*")
 for f in $FILES
 do
     echo $f
-    ipython nbconvert $f --to rst --stdout 2>> /dev/null 1> "${f%.*}.txt"
+    ipython nbconvert $f --to rst --stdout > "${f%.*}.txt"
+    # ipython nbconvert $f --to rst --stdout 2>> /dev/null 1> "${f%.*}.txt"
 done
